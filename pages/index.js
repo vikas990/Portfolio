225e475e1182d040baaf7fa7ec0d503b6894daad
vikas990 <vikaskumarp66@gmail.com>
2022-09-29 21:17:33 +0530

    Added Footer

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
 import Projects from "../components/Projects/Projects";
-import Skill from "../components/skill/skill";
-import Testimonial from "../components/Testimonials/Testimonial";
+import Skill from "../components/skill/Skill";
 
 export default function Home({ setTheme, theme }) {
   return (
